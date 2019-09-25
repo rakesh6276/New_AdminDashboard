9632565a39a73231b9a77fa60b175d0e4252aab3
asm-bay <sunilkumar.pm@asmltd.com>
2019-09-25 11:51:40 +0530

    changed in consoles color

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -14,9 +14,9 @@ import { CookieService } from 'ngx-cookie-service';
 export class DataService {
 
 
-  //apiRoot: string = "http://127.0.0.1:8001 ";
+  //apiRoot: string = "http://152.135.122.61:8871 ";
 
-  apiRoot: string = "http://127.0.0.1:8001";
+  apiRoot: string = "http://152.135.122.61:8871";
   
   //apiRoot: string = "http://127.0.0.1:8000";
 
